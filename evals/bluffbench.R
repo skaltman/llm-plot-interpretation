1b0e72f2a95865e2aaa4aa8d2208b554dc27e022
skaltman <sara.altman@posit.co>
2025-11-14 14:50:45 -0800

    add thinking scripts

--- evals/bluffbench.R
+++ evals/bluffbench.R
@@ -1,23 +1,35 @@
 # Bluffbench eval using native bluffbench package
 
-# Load bluffbench package
-devtools::load_all("/Users/saraa/GitHub/rstudio/bluffbench")
+# With memo prompt and thinking --------------------
+
+library(bluffbench)
+
+vitals::vitals_log_dir_set("./logs/bluffbench")
 
 # Read memo prompt
-prompt <- readr::read_file("prompts/prompt-memo.md")
+prompt <- readr::read_file("prompts/prompt-thinking.md")
 
-tsk <- bluff_task(epochs = 3)
+mocked_samples <- which(bluff_dataset$type == "mocked")
+
+tsk <- bluff_task(epochs = 3, samples = mocked_samples)
 
 # claude 4.5 sonnet -------------------------------------------------
-tsk_claude_4_5_sonnet_prompt_memo <- tsk$clone()
-tsk_claude_4_5_sonnet_prompt_memo$eval(
+tsk_claude_4_5_sonnet_prompt_thinking <- tsk$clone()
+tsk_claude_4_5_sonnet_prompt_thinking$eval(
   solver_chat = ellmer::chat_anthropic(
     model = "claude-sonnet-4-5-20250929",
-    system_prompt = prompt
+    system_prompt = prompt,
+    api_args = list(
+          thinking = list(type = "enabled", budget_tokens = 5000),
+          max_tokens = 8192
+    )
   )
 )
 
-saveRDS(tsk_claude_4_5_sonnet_prompt_memo, file = "results/bluffbench-prompt-memo/tsk_claude_4_5_sonnet.rds")
+saveRDS(
+  tsk_claude_4_5_sonnet_prompt_thinking, 
+  file = "results/bluffbench-prompt-memo/tsk_claude_4_5_sonnet_thinking_mocked.rds"
+)
 
 cli::cli_inform("Sonnet finished.")
 
@@ -30,7 +42,7 @@ tsk_gemini_2_5_pro_prompt_memo$eval(
   )
 )
 
-saveRDS(tsk_gemini_2_5_pro_prompt_memo, file = "results/bluffbench-prompt-memo/tsk_gemini_2_5_pro.rds")
+saveRDS(tsk_gemini_2_5_pro_prompt_memo, file = "results/bluffbench-prompt-memo/tsk_gemini_2_5_pro_thinking.rds")
 
 cli::cli_inform("Gemini finished.")
 
